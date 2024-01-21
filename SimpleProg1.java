794e7d6954066d9cb9d6fdbf40d83feb8be3d143
Paul Franklin <paul.franklin@csuglobal.edu>
2024-01-20 20:19:19 -0500

    location variables declared

--- SimpleProg1.java
+++ SimpleProg1.java
@@ -2,6 +2,9 @@ public class SimpleProg1 {
     public static void main(String[] args) {
         var first_name = "Tom";
         var last_name = "Martin";
+        var address = "2569 White Rock Avenue";
+        var city = "Middletown";
+        var ZIP_code = "06457";
 
 
     }
